8324778f7371ce3690e0d162aaff5b45942d26c7
Matthew <matthewreed.dev@gmail.com>
2023-06-29 01:28:26 -0600

    my first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
      <div className='flex flex-row items-center justify-center text-center text-red-500 border border-indigo-600 text-9xl'>
-      Hello world
+      Hello world {`=]`}
      </div>
     </main>
   )
