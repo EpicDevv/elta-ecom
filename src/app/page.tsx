35d0ca4e6c6e51de30a4a7daf41a630ee72d2c39
zayne.lovecraft <zayne.mngmt@gmail.com>
2023-06-28 23:59:45 -0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+     <div className='flex flex-row items-center justify-center text-center text-red-500 border border-indigo-600 text-9xl'>
+      Hello world
+     </div>
+    </main>
+  )
+}
